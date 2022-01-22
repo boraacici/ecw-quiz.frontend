5507568122f001f76f8889f44323eb02de9ea67d
Bora Acici <sd.bora.acici@gmail.com>
2022-01-22 22:19:20 +0300

    Revert "test: game page height"
    This reverts commit f4ca849d65abb71877acca3128b600937fd86c5d.

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -54,20 +54,8 @@
 
 <script>
 import Timer from '../component/Timer.vue'
-
 export default {
   components: { Timer },
-  mounted() {
-    if (this.isTouchDevice()) {
-    this.$refs.container.style.height = (window.innerHeight) + 'px'
-    document.body.style.height = window.innerHeight + 'px'
-    }
-  },
-  methods: {
-    isTouchDevice() {
-      return window.matchMedia('(pointer: coarse)').matches
-    },
-  },
 }
 </script>
 
