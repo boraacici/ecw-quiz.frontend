63d7a26f660e2dd591b82c042ef6565fc8ce63a7
YalcinKuntaySimsek <kuntayyalcin.simsek@gmail.com>
2022-01-22 17:14:56 +0300

    login page new version

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="login-container">
     <div class="login-box">
-      <h1 class="title">Welcome</h1>
-      <form>
-        <label for="fname">First Name</label>
-        <input
-          type="text"
-          id="fname"
-          name="firstname"
-          placeholder="Your name.."
-        />
-
-        <label for="country">Country</label>
-        <input
-          type="text"
-          id="country"
-          name="country"
-          placeholder="Country"
-        />
-        <input type="submit" value="LOGIN" />
+      <span class="title">English Common Words Quiz</span>
+      <form action="">
+        <label class="name-label" for="fname">Fullname</label>
+        <input class="name-input" type="text" placeholder="Enter your name" />
       </form>
+      <div class="rules">
+        <span class="rules-title">Rules</span>
+        <span class="rule"
+          >1. You will have only 15 seconds per each question.</span
+        >
+        <span class="rule">2. Choose the correct answer and send.</span>
+        <span class="rule"
+          >3. The quiz ends when the wrong answer is given or the time runs
+          out.</span
+        >
+      </div>
+      <div class="start-quiz">
+        <button class="start-quiz-button">Start Quiz</button>
+      </div>
     </div>
   </div>
 </template>
@@ -35,56 +35,106 @@ export default {};
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #5765f2;
-}
+  background: #e5e5e5;
 
-h1{
-  color: #fff;
-}
+  .login-box {
+    width: 569px;
+    height: 709px;
+    background: #ffffff;
+    box-shadow: 0px 15px 40px 5px #ededed;
+    border-radius: 30px;
 
-.login-box {
-  padding: 60px;
-  background: #36393e;
-  border-radius: 10px;
-  max-width: 375px;
-  box-sizing: border-box;
-}
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
 
-.form-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+    .title {
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 30px;
+      line-height: 45px;
+      color: #000000;
+      margin-top: 50px;
+      margin-bottom: 33px;
+    }
+    form {
+      width: 70%;
+      display: inline-flex;
+      flex-direction: column;
+    }
+    .name-label {
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 24px;
+      color: #696f79;
+      margin-bottom: 12px;
+    }
+    .name-input {
+      background: #ffffff;
+      box-shadow: 0px 15px 40px 5px #ededed;
+      border-radius: 30px;
+      border: none;
 
-input {
-  margin-top: 10px;
-}
+      font-family: Poppins;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 21px;
+      color: #8692a6;
+      padding-left: 38px;
+      padding-top: 22px;
+      padding-bottom: 22px;
+    }
+    .rules {
+      max-width: 411px;
+      display: inline-flex;
+      flex-direction: column;
+      margin-top: 56px;
+      margin-bottom: 52px;
 
-form {
-  margin-top: 10px;
-}
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+      .rules-title {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 23px;
+        line-height: 34px;
+        color: #696f79;
+      }
+      .rule {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 37px;
+        color: #8692a6;
+      }
+    }
+    .start-quiz {
+      width: 100%;
+      display: inline-flex;
+      justify-content: flex-end;
 
-input[type=submit] {
-  width: 100%;
-  background-color: #4752c4;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-label{
-  color: #B8BBBE;
-  font-size: 16px;
+      .start-quiz-button {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 22px;
+        line-height: 33px;
+        color: #ffffff;
+
+        background: #8692a6;
+        border-radius: 30px;
+
+        padding: 16px 51px;
+        border: none;
+        cursor: pointer;
+
+        margin-right: 72px;
+      }
+    }
+  }
 }
 </style>
